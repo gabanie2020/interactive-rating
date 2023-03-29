22dd6c38a0c90dd17831f7eb860c7287cc76d975
unknown <gabanie2020@gmail.com>
2023-03-29 09:34:05 +0100

    my second commit

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const mainBox = document.querySelectorAll('.main-box')
-const Thankyou = document.querySelectorAll(".Thank-you")
+const mainBox = document.querySelector('.main-box')
+const ThankyouContainer = document.querySelector(".Thank-you")
 const submitButton = document.getElementById("submit")
 const rateAgain = document.getElementById("rate-again")
 const rating = document.getElementById("rating")
 const rates = document.querySelectorAll("btn")
 
 submitButton.addEventListener("click", () => {
-  Thankyou.classList.remove("hidden")
+  ThankyouContainer.classList.remove ("hidden")
   mainBox.style.display = "none"
 })
 
 rateAgain.addEventListener("click", () => {
-    Thankyou.classList.add("hidden")
+    ThankyouContainer.classList.add("hidden")
     mainBox.style.display = "block"
   })
 
